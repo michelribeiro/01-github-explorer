77fe07ee61df3322c42b17dd9e4cf725bd35bebd
Michel Lima Ribeiro <michelribeiro2004@gmail.com>
2021-07-15 22:23:56 -0300

    transformando js em tsx

--- src/App.jsx
+++
@@ -1,3 +0,0 @@
-export function App() {
-  return <h1>Hello World App mother fuck!!!</h1>;
-}
---
+++ src/components/RepositoryItem.tsx
@@ -0,0 +1,17 @@
+type RepositoriesProps = {
+  repository: {
+    name: string;
+    description: string;
+    html_url: string;
+  };
+};
+
+export function RepositoryItem(props: RepositoriesProps) {
+  return (
+    <li>
+      <strong>{props.repository.name}</strong>
+      <p>{props.repository.description}</p>
+      <a href={props.repository.html_url}>Acessar repositório</a>
+    </li>
+  );
+}
